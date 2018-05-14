00205956f22f00608960c65ff561e6905115158d
Daniel S. Reichenbach <daniel@kogitoapp.com>
2018-05-14 20:55:24 +0200

    Added Docker environment for Symfony flex

---
+++ docker/node/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- yarn "$@"
+fi
+
+if [ "$1" = 'yarn' ]; then
+    # The first time volumes are mounted, the project needs to be recreated
+    if [ ! -f package.json ]; then
+        # Create a blank project
+		yarn init -yp
+		yarn add @symfony/webpack-encore --dev
+    elif [ "$APP_ENV" != 'prod' ]; then
+        # Always try to reinstall deps when not in prod
+        yarn install
+    fi
+fi
+
+exec "$@"
